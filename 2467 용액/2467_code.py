91cd2a1c28a23cf49b41a213d6f0c41037bc8bbe
euichan <dmlcks1998@naver.com>
2024-08-27 11:53:02 +0900

    백준 용액 끝

--- 2467 용액/2467_code.py
+++ 2467 용액/2467_code.py
@@ -1,1 +1,22 @@
 # https://www.acmicpc.net/problem/2467
+import sys
+sys.stdin = open('input_2467.txt','r')
+
+T = 2
+for test_case in range(1,T+1):
+    N = int(input())
+    temp = list(map(int,input().split()))
+    start = 0
+    end = N-1
+    min_sum = float('inf')
+    while True:
+        if end <= start:
+            break
+        sample_sum = temp[start] + temp[end]
+        if abs(sample_sum) < min_sum:
+            min_sum = abs(sample_sum)
+            ans = [temp[start],temp[end]]
+        if sample_sum < 0: start +=1
+        else: end -= 1
+
+    print(*ans)
