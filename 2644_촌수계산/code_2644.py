a49aa44401d4cfa41e85a66e504199f00e8abb3b
euichan <dmlcks1998@naver.com>
2024-08-30 11:49:49 +0900

    백준 촌수계산 끝

--- 2644_촌수계산/code_2644.py
+++ 2644_촌수계산/code_2644.py
@@ -1,1 +1,36 @@
 # https://www.acmicpc.net/problem/2644
+import sys
+sys.stdin = open('input_2644.txt','r')
+
+from collections import  deque
+
+T = 2
+
+def BFS(start,end):
+    d_que = deque()
+    d_que.append((a,1))
+    while d_que:
+        now,cnt = d_que.popleft()
+        visited.add(now)
+        for next_family in family[now]:
+            if next_family == end: return cnt
+            if next_family in visited: continue
+            d_que.append((next_family,cnt+1))
+    return -1
+
+
+for test_case in range(1,T+1):
+    N = int(input())
+    a,b = map(int,input().split())
+    M = int(input())
+    family ={}
+    for i in range(1,N+1):
+        family[i] = set()
+    for _ in range(M):
+        p,c =map(int,input().split())
+        family[p].add(c)
+        family[c].add(p)
+    visited = set()
+    a = BFS(a,b)
+    print(a)
+
