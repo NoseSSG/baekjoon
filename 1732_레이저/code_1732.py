33ecc54fa6cd46ec921b0aefaa7bfd095d4e13e1
dmlcks1998 <dmlcks1998@naver.com>
2024-09-02 14:41:34 +0900

    백준 레이저 테케가 부족 틀렸습니다

--- 1732_레이저/code_1732.py
+++ 1732_레이저/code_1732.py
@@ -1,1 +1,27 @@
 # https://www.acmicpc.net/problem/1732
+import sys
+sys.stdin = open('input_1732.txt','r')
+
+N = int(input())
+degree = {}
+ans = set()
+for _ in range(N):
+    x,y,d = map(int,input().split())
+    deg = y/x
+
+    if deg not in degree:
+        degree[deg] = {d:(x,y)}
+    else:
+        if d not in degree[deg]:
+            degree[deg][d] = (x,y)
+        else:
+            if abs(sum(degree[deg][d])) > abs(x+y):
+                ans.add(degree[deg][d])
+            else:
+                ans.add((x,y))
+answer = list(ans)
+answer.sort()
+for i in answer:
+    print(*i)
+
+
