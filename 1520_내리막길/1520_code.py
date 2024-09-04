0d9deaa3294de03c58333628b5ecb83540f70438
dmlcks1998 <dmlcks1998@naver.com>
2024-09-04 12:47:46 +0900

    백준 내리막길 수정

--- 1520_내리막길/1520_code.py
+++ 1520_내리막길/1520_code.py
@@ -3,26 +3,31 @@ import sys
 sys.stdin = open('input_1520.txt','r')
 
 def DFS(x,y):
-    global ans
+    # 목표 지점에 도착하면 1을 반환 = 도착할 수 있다는 뜻
     if x == N-1 and y == M-1:
         return 1
-    if visited[x][y] != -1:
-        return visited[x][y]
-    visited[x][y] = 0
+    # visited에 좌표 값이 있다면 방문한 적이 있다는 뜻 값 반환
+    if (x,y) in visited:
+        return visited[(x,y)]
+
+    # 0으로 초기화 하여 방문한것을 표시함
+    visited[(x,y)] = 0
     for d in range(4):
         now = groud[x][y]
         nx, ny = x+dx[d], y+dy[d]
         if 0<= nx < N and 0<= ny < M and now > groud[nx][ny]:
-            visited[x][y] += DFS(nx,ny)
-    return visited[x][y]
+            # 이동 가능한 좌표들을 DFS를 통해 목표 지점에 도착가능한 모든 경우를 더해준다
+            visited[(x,y)] += DFS(nx,ny)
+
+    return visited[(x,y)]
 
 
 N,M = map(int,input().split())
 groud = [list(map(int,input().split())) for _ in range(N)]
 dx = [1,0,-1,0]
 dy = [0,1,0,-1]
-visited = [[-1] * M for _ in range(N)]
+# 방문 여부를 확인하기 위한 딕셔너리
+visited = {}
 
-ans = 0
-
-print(DFS(0,0))
+DFS(0,0)
+print(visited[(0,0)])
