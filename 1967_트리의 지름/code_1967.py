d21523065f10af179c5e99fe97cf78209f15d4d6
NoseSSG <dmlcks1998@naver.com>
2024-09-06 00:55:10 +0900

    백준 트리의 지름 끝

--- 1967_트리의 지름/code_1967.py
+++ 1967_트리의 지름/code_1967.py
@@ -1,1 +1,34 @@
 # https://www.acmicpc.net/problem/1967
+import sys
+sys.stdin = open('input_1967.txt','r')
+
+from collections import deque
+def BFS(start):
+    d_que = deque()
+    d_que.append((start,0))
+    while d_que:
+        now,dist = d_que.popleft()
+        visited[now] = dist
+        if now not in tree:continue
+        for next,edge in tree[now]:
+            if visited[next] == -1:
+
+                visited[next] = dist + edge
+                d_que.append((next,visited[next]))
+
+N = int(input())
+tree = {}
+for _ in range(N-1):
+    a,b,c = map(int,input().split())
+    if a not in tree:
+        tree[a] = set()
+    if b not in tree:
+        tree[b] = set()
+    tree[a].add((b,c))
+    tree[b].add((a,c))
+visited = [-1] * (N+1)
+BFS(1)
+max_idx = visited.index(max(visited))
+visited = [-1] * (N+1)
+BFS(max_idx)
+print(max(visited))
