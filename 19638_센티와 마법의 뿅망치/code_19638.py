135ae5d717f57aa3311eafb63bbc8a8c5f7c4955
NoseSSG <dmlcks1998@naver.com>
2024-09-06 01:11:36 +0900

    백준 센티와 마법의 뿅망치 끝

--- 19638_센티와 마법의 뿅망치/code_19638.py
+++ 19638_센티와 마법의 뿅망치/code_19638.py
@@ -1,1 +1,24 @@
 # https://www.acmicpc.net/problem/19638
+import sys
+# sys.stdin = open('input_19638.txt','r')
+import heapq
+N, H, T = map(int,input().split())
+temp = []
+cnt = 0
+for _ in range(N):
+    heapq.heappush(temp,-int(input()))
+for i in range(T):
+    height = -heapq.heappop(temp)
+    if height < H or height==1:
+        heapq.heappush(temp,-height)
+        break
+    height //= 2
+    heapq.heappush(temp,-height)
+    cnt += 1
+max_height = -heapq.heappop(temp)
+if max_height >= H:
+    print('NO')
+    print(max_height)
+else:
+    print('YES')
+    print(cnt)
