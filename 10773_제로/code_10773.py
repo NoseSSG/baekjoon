69ed4abd50a2620df5e4dff20cd743572230fd85
NoseSSG <dmlcks1998@naver.com>
2024-09-05 23:37:56 +0900

    백준 제로 끝

--- 10773_제로/code_10773.py
+++ 10773_제로/code_10773.py
@@ -1,1 +1,14 @@
 # https://www.acmicpc.net/problem/10773
+
+import sys
+sys.stdin = open('input_10773.txt','r')
+
+N = int(input())
+temp = []
+for _ in range(N):
+    t = int(input())
+    if t == 0:
+        temp.pop()
+    else:
+        temp.append(t)
+print(sum(temp))
