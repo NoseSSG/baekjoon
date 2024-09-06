be9e1293742f6bfa9954d9e566cee41759203628
NoseSSG <dmlcks1998@naver.com>
2024-09-07 00:03:58 +0900

    백준 문자열 집합 끝

--- 14425_문자열 집합/code_14425.py
+++ 14425_문자열 집합/code_14425.py
@@ -1,1 +1,15 @@
 # https://www.acmicpc.net/problem/14425
+import sys
+sys.stdin = open('input_14425.txt','r')
+
+N,M = map(int,input().split())
+string_set = set()
+ans = 0
+for _ in range(N):
+    temp = input()
+    string_set.add(temp)
+for _ in range(M):
+    temp = input()
+    if temp in string_set:
+        ans += 1
+print(ans)
