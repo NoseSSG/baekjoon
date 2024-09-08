ffe66ffce0f530947e4c4c3fd03ad3b31c288018
NoseSSG <dmlcks1998@naver.com>
2024-09-09 00:32:38 +0900

    백준 큐빙 끝

--- 5373_큐빙/code_5373.py
+++ 5373_큐빙/code_5373.py
@@ -15,20 +15,20 @@ for test_case in range(1,T+1):
         if o[0] == 'L':
             if o[1] =='+':
                 for i in range(3):
-                    cube[0][i][0],cube[1][i][0],cube[2][i][0],cube[3][2-i][2] = cube[3][2-i][2],cube[0][i][0],cube[1][i][0],cube[2][i][0]
+                    cube[0][i][0],cube[1][i][0],cube[2][i][0],cube[3][2-i][0] = cube[3][2-i][0],cube[0][i][0],cube[1][i][0],cube[2][i][0]
                 cube[4] = list(map(list,zip(*cube[4][::-1])))
             elif o[1] == '-':
                 for i in range(3):
-                    cube[0][i][0],cube[1][i][0],cube[2][i][0],cube[3][2-i][2] = cube[1][i][0],cube[2][i][0],cube[3][2-i][2],cube[0][i][0]
+                    cube[0][i][0],cube[1][i][0],cube[2][i][0],cube[3][2-i][0] = cube[1][i][0],cube[2][i][0],cube[3][2-i][0],cube[0][i][0]
                 cube[4] = list(map(list, zip(*cube[4])))[::-1]
         elif o[0] == 'R':
             if o[1] == '+':
                 for i in range(3):
-                    cube[0][i][2], cube[1][i][2], cube[2][i][2], cube[3][2-i][0] = cube[1][i][2], cube[2][i][2], cube[3][2-i][0],cube[0][i][2]
+                    cube[0][i][2], cube[1][i][2], cube[2][i][2], cube[3][2-i][2] = cube[1][i][2], cube[2][i][2], cube[3][2-i][2],cube[0][i][2]
                 cube[5] = list(map(list, zip(*cube[5][::-1])))
             elif o[1] == '-':
                 for i in range(3):
-                    cube[0][i][2], cube[1][i][2], cube[2][i][2], cube[3][2-i][0] = cube[3][2-i][0],cube[0][i][2], cube[1][i][2], cube[2][i][2]
+                    cube[0][i][2], cube[1][i][2], cube[2][i][2], cube[3][2-i][2] = cube[3][2-i][2],cube[0][i][2], cube[1][i][2], cube[2][i][2]
                 cube[5] = list(map(list, zip(*cube[5])))[::-1]
         elif o[0] == 'U':
             if o[1] == '+':
@@ -54,25 +54,25 @@ for test_case in range(1,T+1):
         elif o[0] == 'F':
             if o[1] == '+':
                 for i in range(3):
-                    cube[0][2][i],cube[5][i][0],cube[2][0][i],cube[4][2-i][2] = cube[4][2-i][2],cube[0][2][i],cube[5][i][0],cube[2][0][i]
+                    cube[0][2][i],cube[5][i][0],cube[2][0][2-i],cube[4][2-i][2] = cube[4][2-i][2],cube[0][2][i],cube[5][i][0],cube[2][0][2-i]
                 cube[1] = list(map(list, zip(*cube[1][::-1])))
             elif o[1] == '-':
                 for i in range(3):
-                    cube[0][2][i],cube[5][i][0],cube[2][0][i],cube[4][2-i][2] = cube[5][i][0],cube[2][0][i],cube[4][2-i][2],cube[0][2][i]
+                    cube[0][2][i],cube[5][i][0],cube[2][0][2-i],cube[4][2-i][2] = cube[5][i][0],cube[2][0][2-i],cube[4][2-i][2],cube[0][2][i]
                 cube[1] = list(map(list, zip(*cube[1])))[::-1]
         elif o[0] == 'B':
             if o[1] == '+':
                 for i in range(3):
-                    cube[0][0][i], cube[5][i][2],cube[2][2][i],cube[4][2-i][0] = cube[5][i][2],cube[2][2][i],cube[4][2-i][0],cube[0][0][i]
-                cube[3] = list(map(list, zip(*cube[3][::-1])))
+                    cube[0][0][i], cube[5][i][2],cube[2][2][2-i],cube[4][2-i][0] = cube[5][i][2],cube[2][2][2-i],cube[4][2-i][0],cube[0][0][i]
+                cube[3] = list(map(list, zip(*cube[3])))[::-1]
             elif o[1] == '-':
                 for i in range(3):
-                    cube[0][0][i], cube[5][i][2],cube[2][2][i],cube[4][2-i][0] = cube[4][2-i][0],cube[0][0][i], cube[5][i][2],cube[2][2][i]
-                cube[3] = list(map(list, zip(*cube[3])))[::-1]
+                    cube[0][0][i], cube[5][i][2],cube[2][2][2-i],cube[4][2-i][0] = cube[4][2-i][0],cube[0][0][i], cube[5][i][2],cube[2][2][2-i]
+                cube[3] = list(map(list, zip(*cube[3][::-1])))
         # for c in cube:
         #     for t in c:
         #         print(*t)
         #     print()
         # print('-------------')
     for t in cube[0]:
-        print(*t)
+        print(*t,sep='')
