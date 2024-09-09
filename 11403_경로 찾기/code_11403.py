2b660121ae3280c7a52034583b131ea6f7ebc0ee
dmlcks1998 <dmlcks1998@naver.com>
2024-09-09 14:12:51 +0900

    백준 경로찾기 끝

--- 11403_경로 찾기/code_11403.py
+++ 11403_경로 찾기/code_11403.py
@@ -1,1 +1,25 @@
 # https://www.acmicpc.net/problem/11403
+import sys
+# sys.stdin = open('input_11403.txt','r')
+
+from collections import deque
+
+def BFS(start):
+    visited = [0]* N
+    temp = deque()
+    temp.append(start)
+    while temp:
+        now = temp.popleft()
+        for idx in range(N):
+            if edge[now][idx] == 1 and visited[idx] == 0:
+                visited[idx] = 1
+                temp.append(idx)
+    ans.append(visited)
+
+N = int(input())
+edge = [list(map(int,input().split())) for _ in range(N)]
+ans = []
+for i in range(N):
+    BFS(i)
+for ii in ans:
+    print(*ii)
