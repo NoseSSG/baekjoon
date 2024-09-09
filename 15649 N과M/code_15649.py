8fd087bc6bf8dc1d25ce08bda99aa1b1dacbf44f
dmlcks1998 <dmlcks1998@naver.com>
2024-09-09 09:32:43 +0900

    백준 N과M 끝

---
+++ 15649 N과M/code_15649.py
@@ -0,0 +1,16 @@
+# https://www.acmicpc.net/problem/15649
+import sys
+# sys.stdin = open('input_15649.txt','r')
+
+# def DFS(select,not_select):
+#     if len(select)==M:
+#         print(*select)
+#         return
+#     for idx in range(len(not_select)):
+#         DFS(select +[not_select[idx]],not_select[:idx]+not_select[idx+1:])
+#
+# N,M = map(int,input().split())
+# DFS([],[i for i in range(1,N+1)])
+temp = [i for i in range(10)]
+ans = ' '.join(map(str,temp))
+print(ans)
