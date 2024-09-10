261310fb8971f3667b3b7bfbdba7762b2341b191
NoseSSG <dmlcks1998@naver.com>
2024-09-10 21:53:36 +0900

    백준 N과M(4) 끝

--- 15652_N과M(4)/code_15652.py
+++ 15652_N과M(4)/code_15652.py
@@ -1,1 +1,13 @@
 # https://www.acmicpc.net/problem/15652
+import sys
+# sys.stdin = open('input_15652.txt','r')
+
+def DFS(select,not_select):
+    if len(select)==M:
+        print(*select)
+        return
+    for idx in range(len(not_select)):
+        DFS(select +[not_select[idx]],not_select[idx:])
+
+N,M = map(int,input().split())
+DFS([],[i for i in range(1,N+1)])
