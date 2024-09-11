3a39ea1aafcb4dbbb6a4320713951d5b68e95892
euichan <dmlcks1998@naver.com>
2024-09-11 13:32:59 +0900

    백준 후위 표기식 끝

--- 1918_후위 표기식/code_1918.py
+++ 1918_후위 표기식/code_1918.py
@@ -1,1 +1,28 @@
 # https://www.acmicpc.net/problem/1918
+import sys
+sys.stdin = open('input_1918.txt','r')
+
+line = input()
+ans =''
+temp = []
+for i in line:
+    if i == '(':
+        temp.append(i)
+    elif i == ')':
+        while temp and temp[-1] != '(':
+            ans += temp.pop()
+        temp.pop()
+    elif i == '*' or i == '/':
+        while temp and (temp[-1] == '/' or temp[-1] == '*'):
+            ans += temp.pop()
+        temp.append(i)
+    elif i == '+' or i == '-':
+        while temp and temp[-1] != '(':
+            ans += temp.pop()
+        temp.append(i)
+    else:
+        ans += i
+    # print(temp,ans)
+while temp:
+    ans += temp.pop()
+print(ans)
