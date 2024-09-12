ae82743b4fbddf0226ed80ea3a6453df28868137
euichan <dmlcks1998@naver.com>
2024-09-12 10:01:55 +0900

    백준 키 포기

--- 1129_키/code_1129.py
+++ 1129_키/code_1129.py
@@ -1,30 +1,4 @@
-# https://www.acmicpc.net/problem/1129
-import sys
-# sys.stdin = open('input_1129.txt','r')
+# # https://www.acmicpc.net/problem/1129
+# 내 실력이 부족하여 해결하지 못함
 
 
-from collections import deque
-
-N = int(input())
-temp = list(map(int,input().split()))
-temp.sort()
-ans1 = []
-ans2 = []
-ans1.append(temp[0])
-ans2.append(temp[0])
-flag = 0
-for i in temp[1:]:
-    if i - ans1[-1]
-    if flag == 0:
-        ans1.append(i)
-    else:
-        ans2.append(i)
-
-ans2.reverse()
-ttt = ans1+ ans2[:-1]
-d_que = deque(ttt)
-min_ans = ttt
-for _ in range(N):
-    d_que.rotate(1)
-    min_ans = min(min_ans,list(d_que))
-print(*min_ans)
