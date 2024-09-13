b45fdce24f8dfbabecc916cc3cb0e79f5a4167e5
euichan <dmlcks1998@naver.com>
2024-09-13 14:02:34 +0900

    트리순회 풀이중

--- 1991_트리순회/code_1991.py
+++ 1991_트리순회/code_1991.py
@@ -1,1 +1,38 @@
 # https://www.acmicpc.net/problem/1991
+import sys
+sys.stdin = open('input_1991.txt')
+
+from collections import deque
+
+
+def pre_ord(start):
+    ans = ''
+    temp = deque()
+    temp.append(start)
+    while temp:
+        now = temp.popleft()
+        ans += now
+        if now in graph:
+
+    pass
+
+
+def in_ord(start):
+    pass
+
+def posr_ord(start):
+    pass
+
+
+
+graph = {}
+N = int(input())
+for _ in range(N):
+    a,b,c = map(str,input().split())
+    if a not in graph:
+        graph[a] = []
+    if b != '.':
+        graph[a].append(b)
+    if c != '.':
+        graph[a].append(c)
+
