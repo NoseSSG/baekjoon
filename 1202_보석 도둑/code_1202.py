d38cb2b412f53eb7ef728b2b2a8abb59fd780778
euichan <dmlcks1998@naver.com>
2024-09-13 13:19:35 +0900

    백준 보석 도둑 끝

--- 1202_보석 도둑/code_1202.py
+++ 1202_보석 도둑/code_1202.py
@@ -6,28 +6,21 @@ import heapq
 
 N,K = map(int,input().split())
 h_que = []
-use = [False]*N
-ans = [0]*N
-max_idx = 0
 for _ in range(N):
     a,b = map(int,input().split())
-    # heapq.heappush(h_que,(a,b))
-    h_que.append((a,b))
-h_que.sort()
+    heapq.heappush(h_que,(a,b))
+
 bag = []
 for _ in range(K):
     c = int(input())
     bag.append(c)
-bag.sort(reverse=True)
-for bag_idx in range(K):
-    max_idx = 0
-    bag_size = bag[bag_idx]
-    for idx in range(N):
-        if use[idx]: continue
-        if h_que[idx][0] > bag_size:
-            break
-        if ans[bag_idx] <= h_que[idx][1]:
-            ans[bag_idx] = h_que[idx][1]
-            max_idx = idx
-    use[max_idx] = True
-print(sum(ans))
+bag.sort()
+choice = []
+ans = 0
+for select_bag in bag:
+    while h_que and h_que[0][0] <= select_bag:
+        heapq.heappush(choice,-heapq.heappop(h_que)[1])
+    if choice:
+        ans += -heapq.heappop(choice)
+
+print(ans)
