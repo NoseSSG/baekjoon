f74b9fb086f56ead86a61acdd89ad75fc65aa130
euichan <dmlcks1998@naver.com>
2024-09-14 17:13:26 +0900

    백준 츠트리 순회ã 끝

--- 1991_트리순회/code_1991.py
+++ 1991_트리순회/code_1991.py
@@ -1,27 +1,28 @@
 # https://www.acmicpc.net/problem/1991
 import sys
-sys.stdin = open('input_1991.txt')
-
-from collections import deque
-
+# sys.stdin = open('input_1991.txt')
 
 def pre_ord(start):
-    ans = ''
-    temp = deque()
-    temp.append(start)
-    while temp:
-        now = temp.popleft()
-        ans += now
-        if now in graph:
-
-    pass
+    if start == '.':
+        return
+    print(start,end='')
+    pre_ord(graph[start][0])
+    pre_ord(graph[start][1])
 
 
 def in_ord(start):
-    pass
+    if start == '.':
+        return
+    in_ord(graph[start][0])
+    print(start,end='')
+    in_ord(graph[start][1])
 
-def posr_ord(start):
-    pass
+def post_ord(start):
+    if start == '.':
+        return
+    post_ord(graph[start][0])
+    post_ord(graph[start][1])
+    print(start, end='')
 
 
 
@@ -31,8 +32,12 @@ for _ in range(N):
     a,b,c = map(str,input().split())
     if a not in graph:
         graph[a] = []
-    if b != '.':
-        graph[a].append(b)
-    if c != '.':
-        graph[a].append(c)
+    graph[a].append(b)
+    graph[a].append(c)
 
+pre_ord('A')
+print()
+in_ord('A')
+print()
+post_ord('A')
+print()
