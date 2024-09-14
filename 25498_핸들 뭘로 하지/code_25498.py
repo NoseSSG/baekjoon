63c5c98d3627754b3cb8be5227f4f54ccde70abf
euichan <dmlcks1998@naver.com>
2024-09-14 21:11:39 +0900

    백준 핸들뭘로하지 포기

--- 25498_핸들 뭘로 하지/code_25498.py
+++ 25498_핸들 뭘로 하지/code_25498.py
@@ -1,34 +1,46 @@
 # https://www.acmicpc.net/problem/25498
 import sys
-sys.stdin = open('input_25498.txt','r')
+# sys.stdin = open('input_25498.txt','r')
 from collections import deque
 
-def DFS(node,temp):
+def BFS(start):
     global ans
-    visited.add(node)
     d_que = deque()
-    d_que.append((node,temp))
-    while d_que:
-        idx,str = d_que.popleft()
-        visited.add(idx)
-        max_idx = []
-        max_char = ''
-        for i in graph[idx]:
-            if i in visited: continue
-            if max_char < char[i]:
-                max_idx = [i]
-                max_char = char[i]
-            elif max_char == char[i]:
-                max_idx.append(i)
-        if not max_idx:
-            if ans < str+char[idx]: ans = str+char[idx]
+    d_que.append((start,0))
+    temp_que = deque()
+    while d_que or temp_que:
+        if d_que:
+            now,idx = d_que.popleft()
+            if len(ans) == idx: ans += char[now]
+            visited.add(now)
+            for next_ in graph[now]:
+                if next_ in visited: continue
+                if not temp_que:
+                    temp_que.append((next_,idx+1))
+                else:
+                    if char[temp_que[-1][0]] < char[next_]:
+                        temp_que.clear()
+                        temp_que.append((next_,idx+1))
+                    elif char[temp_que[-1][0]] == char[next_]:
+                        temp_que.append((next_,idx+1))
         else:
-            for i in max_idx:
-                d_que.append((i,str+char[idx]))
+            now, idx = temp_que.popleft()
+            if len(ans) == idx: ans += char[now]
+            visited.add(now)
+            for next_ in graph[now]:
+                if next_ in visited: continue
+                if not d_que:
+                    d_que.append((next_, idx + 1))
+                else:
+                    if char[d_que[-1][0]] < char[next_]:
+                        d_que.clear()
+                        d_que.append((next_, idx + 1))
+                    elif char[d_que[-1][0]] == char[next_]:
+                        d_que.append((next_, idx + 1))
+
 
 
 N = int(input())
-string = set()
 graph = {}
 char = list(map(str,input().rstrip()))
 visited = set()
@@ -37,13 +49,23 @@ for _ in range(N-1):
     a -= 1
     b -= 1
     if a not in graph:
-        graph[a] = set()
+        graph[a] = []
+        graph[a].append(b)
+    else:
+        if char[graph[a][-1]] <= char[b]:
+            if char[graph[a][-1]] == char[b]:
+                graph[a] = [b]
+            else:
+                graph[a].append(b)
     if b not in graph:
-        graph[b] = set()
-    graph[a].add(b)
-    graph[b].add(a)
+        graph[b] = []
+        graph[b].append(a)
+    else:
+        if char[graph[b][-1]] <= char[a]:
+            if char[graph[b][-1]] == char[a]:
+                graph[b] = [a]
+            else:
+                graph[b].append(a)
 ans = ''
-DFS(0,'')
+BFS(0)
 print(ans)
-# answer = sorted(string,reverse=True)
-# print(answer[0])
