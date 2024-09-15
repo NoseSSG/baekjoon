87ea64b0a0d6abaab5781b22b28059578d2dff0d
euichan <dmlcks1998@naver.com>
2024-09-15 09:53:04 +0900

    백준 N과M(5) 끝

--- 15654_N과M(5)/code_15654.py
+++ 15654_N과M(5)/code_15654.py
@@ -1,1 +1,15 @@
 # https://www.acmicpc.net/problem/15654
+import sys
+# sys.stdin = open('input_15654.txt','r')
+
+def DFS(select,not_select):
+    if len(select)==M:
+        print(*select)
+        return
+    for idx in range(len(not_select)):
+        DFS(select +[not_select[idx]],not_select[:idx]+not_select[idx+1:])
+
+N,M = map(int,input().split())
+num = list(map(int,input().split()))
+num.sort()
+DFS([],num)
