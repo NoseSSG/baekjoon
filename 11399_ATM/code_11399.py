58fba2cb786040ede1b558a47cd3877b242a360b
euichan <dmlcks1998@naver.com>
2024-09-16 09:51:15 +0900

    ë°백준 ATM 끝

--- 11399_ATM/code_11399.py
+++ 11399_ATM/code_11399.py
@@ -1,1 +1,13 @@
 # https://www.acmicpc.net/problem/11399
+import sys
+sys.stdin = open('input_11399.txt')
+
+N = int(input())
+num = list(map(int,input().split()))
+num.sort()
+ans = 0
+sum_time =0
+for time in num:
+    ans += sum_time + time
+    sum_time += time
+print(ans)
