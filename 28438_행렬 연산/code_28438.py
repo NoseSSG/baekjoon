4254ed5d4759c669719dc4fcfe3c191f46f5a5e7
NoseSSG <dmlcks1998@naver.com>
2024-09-18 17:14:11 +0900

    백준 형렬 연산 끝

--- 28438_행렬 연산/code_28438.py
+++ 28438_행렬 연산/code_28438.py
@@ -1,1 +1,17 @@
 # https://www.acmicpc.net/problem/28438
+import sys
+sys.stdin = open('input_28439.txt','r')
+
+N,M,Q = map(int,input().split())
+row = [0] * M
+cal = [0] * N
+for _ in range(Q):
+    a,b,c = map(int,input().split())
+    if a == 1:
+        cal[b-1] += c
+    elif a == 2:
+        row[b-1] += c
+for i in range(N):
+    for j in range(M):
+        print(cal[i]+row[j],end=' ')
+    print()
