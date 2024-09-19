9cee28afa17ce6d1549b32476aaed5e78b061133
euichan <dmlcks1998@naver.com>
2024-09-19 16:00:07 +0900

    백준 동전 2 끝

--- 2294_동전 2/code_2294.py
+++ 2294_동전 2/code_2294.py
@@ -1,1 +1,17 @@
 # https://www.acmicpc.net/problem/2294
+import sys
+sys.stdin = open('input_2294.txt')
+
+N ,K = map(int,input().split())
+coin = [ int(input()) for _ in range(N)]
+DP = [float('inf')] * (K+1)
+for c in coin:
+    for idx in range(c,K+1):
+        if c == idx:
+            DP[idx] = 1
+            continue
+        DP[idx] = min(DP[idx],DP[idx-c]+1)
+if DP[K] == float('inf'):
+    print(-1)
+else:
+    print(DP[K])
