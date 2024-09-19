c138e8a09f9d3e4898cb2de8d25ccc0df38a1911
euichan <dmlcks1998@naver.com>
2024-09-19 14:20:08 +0900

    백준 숨바꼭질 끝

--- 1697_숨바꼭질/code_1697.py
+++ 1697_숨바꼭질/code_1697.py
@@ -1,1 +1,21 @@
 # https://www.acmicpc.net/problem/1697
+import sys
+sys.stdin = open('input_1697.txt')
+
+from collections import deque
+
+def BFS(start):
+    d_que = deque()
+    d_que.append(start)
+    time[start] = 0
+    while d_que:
+        now = d_que.popleft()
+        if now == K: return time[now]
+        for i in [now-1,now+1,now*2]:
+            if 0 <= i <= 100000 and time[i] == -1:
+                time[i] = time[now] + 1
+                d_que.append(i)
+
+N,K = map(int,input().split())
+time = [-1] * 100001
+print(BFS(N))
