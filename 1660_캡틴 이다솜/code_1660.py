88c3baced774416d0812b71a5a5132d6b5341599
euichan <dmlcks1998@naver.com>
2024-09-19 15:54:17 +0900

    백준 캡틴 이다솜 끝

--- 1660_캡틴 이다솜/code_1660.py
+++ 1660_캡틴 이다솜/code_1660.py
@@ -1,1 +1,23 @@
 # https://www.acmicpc.net/problem/1660
+import sys
+sys.stdin = open('input_1660.txt')
+
+N = int(input())
+bomb = []
+num = 0
+i = 1
+while num < N:
+    num += (i*(i+1))//2
+    bomb.append(num)
+    i += 1
+
+dp = [float('inf')]*(N+1)
+for cnt in range(1,N+1):
+    for now in bomb:
+        if cnt == now:
+            dp[cnt] = 1
+            break
+        if now > cnt:
+            break
+        dp[cnt] = min(dp[cnt],dp[cnt-now]+1)
+print(dp[N])
