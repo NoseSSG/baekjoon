ebb405be8388461a7d1b46ec69eec801c3e3cf1e
euichan <dmlcks1998@naver.com>
2024-09-20 16:57:18 +0900

    백준 좋다 끝

--- 1253_좋다/code_1253.py
+++ 1253_좋다/code_1253.py
@@ -1,1 +1,27 @@
 # https://www.acmicpc.net/problem/1253
+import sys
+sys.stdin = open('input_125.txt')
+
+N = int(input())
+num = list(map(int,input().split()))
+num.sort()
+ans = 0
+for idx in range(N):
+    start = 0
+    end = N-1
+    while True:
+        if start == end:
+            break
+        if num[start] + num[end] == num[idx]:
+            if start == idx:
+                start += 1
+            elif end == idx:
+                end -= 1
+            else:
+                ans += 1
+                break
+        elif num[start] + num[end] > num[idx]:
+            end -= 1
+        else:
+            start += 1
+print(ans)
