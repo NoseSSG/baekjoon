c2859a103d4c4fb043f8ff0dbdf6c1d9882fd976
euichan <dmlcks1998@naver.com>
2024-09-20 16:39:14 +0900

    백준 문자열 폭발 끝

--- 9935_문자열 폭발/code_9935.py
+++ 9935_문자열 폭발/code_9935.py
@@ -1,1 +1,18 @@
 # https://www.acmicpc.net/problem/9935
+import sys
+# sys.stdin = open('input_9935.txt','r')
+
+first_text = input()
+second_text = input()
+len_fir = len(first_text)
+len_sec = len(second_text)
+ans = []
+for i in range(len(first_text)):
+    ans.append(first_text[i])
+    if len(ans) >= len_sec and ''.join(ans[-len_sec:]) == second_text:
+        for _ in range(len_sec):
+            ans.pop()
+if ans:
+    print(''.join(ans))
+else:
+    print('FRULA')
