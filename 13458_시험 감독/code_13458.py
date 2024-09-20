b7b6e4f0d5c768f5671de7b024f16de3cdd256a5
euichan <dmlcks1998@naver.com>
2024-09-20 15:31:41 +0900

    백준 시험감독 끝

--- 13458_시험 감독/code_13458.py
+++ 13458_시험 감독/code_13458.py
@@ -1,1 +1,17 @@
 # https://www.acmicpc.net/problem/13458
+import sys
+# sys.stdin = open('input_13458.txt','r')
+#
+N = int(input())
+temp = list(map(int,input().split()))
+a,b =map(int,input().split())
+ans = 0
+for now in temp:
+    if now <= a:
+        ans+=1
+    else:
+        now -= a
+        ans += 1
+        if now % b: ans += 1
+        ans += now//b
+print(ans)
