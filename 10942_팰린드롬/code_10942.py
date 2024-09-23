2db79f00454faf15a7cb9a794be506440c2dfcca
euichan <dmlcks1998@naver.com>
2024-09-23 13:51:44 +0900

    백준 팰린드롬? 끝

--- 10942_팰린드롬/code_10942.py
+++ 10942_팰린드롬/code_10942.py
@@ -1,1 +1,24 @@
 # https://www.acmicpc.net/problem/10942
+import sys
+sys.stdin = open('input_10942.txt','r')
+input = sys.stdin.readline
+
+N = int(input())
+num = list(map(int,input().split()))
+DP = [[0] * N for _ in range(N)]
+M = int(input())
+
+for i in range(N):
+    for j in range(N-i):
+        end = j + i
+        if j == end:
+            DP[j][end] = 1
+        elif num[j] == num[end]:
+            if j + 1 == end:
+                DP[j][end] = 1
+            elif DP[j+1][end-1] == 1:
+                DP[j][end] = 1
+
+for _ in range(M):
+    a,b = map(int,input().split())
+    print(DP[a-1][b-1])
