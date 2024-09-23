5e62e4774d30586373b675b1056a085add1290ef
NoseSSG <dmlcks1998@naver.com>
2024-09-23 20:12:58 +0900

    백준 빗물 끝

--- 14719_빗물/code_14719.py
+++ 14719_빗물/code_14719.py
@@ -1,28 +1,18 @@
 # https://www.acmicpc.net/problem/14719
 import sys
-sys.stdin = open('input_14719.txt','r')
+# sys.stdin = open('input_14719.txt','r')
 
 H,W = map(int,input().split())
 block = list(map(int,input().split()))
 ans = 0
-front = rear = 0
-block_sum = 0
 
-for idx in range(W):
-    if block[front]==0:
-        front = idx
-        rear = idx
-        continue
-    rear = idx
-    if front == rear:
-        continue
-    if block[rear] >= block[front]:
-        print(((rear-front-1)*block[rear]))
-        ans += ((rear-front-1)*block[rear]) - block_sum
-        block_sum = 0
-        front = idx
-    else:
-        block_sum += block[idx]
+for idx in range(1,W-1):
+    left = max(block[:idx])
+    right = max(block[idx+1:])
+
+    max_h = min(left,right)
+    if max_h > block[idx]:
+        ans += max_h - block[idx]
 print(ans)
 
 
