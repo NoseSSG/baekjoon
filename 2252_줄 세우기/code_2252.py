057b9c0a380c8b953cfc7e1544769455e1f53312
euichan <dmlcks1998@naver.com>
2024-09-23 11:48:21 +0900

    백준 줄 세우기 끝

--- 2252_줄 세우기/code_2252.py
+++ 2252_줄 세우기/code_2252.py
@@ -1,1 +1,26 @@
 # https://www.acmicpc.net/problem/2252
+import sys
+# sys.stdin = open('input_2252.txt','r')
+
+from collections import deque
+
+N,M = map(int,input().split())
+graph = [[] for _ in range(N+1)]
+edge = [0] *(N+1)
+for _ in range(M):
+    a,b = map(int,input().split())
+    graph[a].append(b)
+    edge[b] += 1
+d_que = deque()
+for idx in range(1,N+1):
+    if edge[idx] == 0:
+        d_que.append(idx)
+ans = []
+while d_que:
+    now = d_que.popleft()
+    ans.append(now)
+    for next in graph[now]:
+        edge[next] -= 1
+        if edge[next] == 0:
+            d_que.append(next)
+print(*ans)
