36dabe6b28bf6db3c38a1d8236aa68361638b541
euichan <dmlcks1998@naver.com>
2024-09-23 17:57:02 +0900

    백준 빗물 풀이중

--- 14719_빗물/code_14719.py
+++ 14719_빗물/code_14719.py
@@ -1,1 +1,30 @@
 # https://www.acmicpc.net/problem/14719
+import sys
+sys.stdin = open('input_14719.txt','r')
+
+H,W = map(int,input().split())
+block = list(map(int,input().split()))
+ans = 0
+front = rear = 0
+block_sum = 0
+
+for idx in range(W):
+    if block[front]==0:
+        front = idx
+        rear = idx
+        continue
+    rear = idx
+    if front == rear:
+        continue
+    if block[rear] >= block[front]:
+        print(((rear-front-1)*block[rear]))
+        ans += ((rear-front-1)*block[rear]) - block_sum
+        block_sum = 0
+        front = idx
+    else:
+        block_sum += block[idx]
+print(ans)
+
+
+
+
