b5c79d6ce5e8d1746a1ecbc0317a4747cd8bd94e
euichan <dmlcks1998@naver.com>
2024-09-24 11:39:19 +0900

    백준 가장 긴 증가하는 부분 수열 2 끝

--- 12015_가장 긴 증가하는 부분 수열 2/code_12015.py
+++ 12015_가장 긴 증가하는 부분 수열 2/code_12015.py
@@ -1,1 +1,16 @@
 # https://www.acmicpc.net/problem/12015
+import sys
+sys.stdin = open('input_12015.txt','r')
+
+import bisect
+
+N = int(input())
+num = list(map(int,input().split()))
+ans = []
+for i in num:
+    idx = bisect.bisect_left(ans,i)
+    if idx == len(ans):
+        ans.append(i)
+    else:
+        ans[idx] = i
+print(len(ans))
