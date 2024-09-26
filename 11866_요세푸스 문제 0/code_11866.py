9e55a40ef2f21cd3419fee8f2b9b91f0702c754b
euichan <dmlcks1998@naver.com>
2024-09-26 10:07:36 +0900

    백준 요세푸스 문제 0 끝

--- 11866_요세푸스 문제 0/code_11866.py
+++ 11866_요세푸스 문제 0/code_11866.py
@@ -1,6 +1,6 @@
 # https://www.acmicpc.net/problem/11866
 import sys
-sys.stdin = open('input_11866.txt','r')
+# sys.stdin = open('input_11866.txt','r')
 
 from collections import deque
 
@@ -8,6 +8,8 @@ N,M = map(int,input().split())
 ans = []
 d_que = deque(list(range(1,N+1)))
 while d_que:
-    d_que.rotate(-2)
+    d_que.rotate(-(M-1))
     ans.append(d_que.popleft())
-print(f'< {d_que}')
+
+ans = ', '.join(map(str,ans))
+print(f'<{ans}>')
