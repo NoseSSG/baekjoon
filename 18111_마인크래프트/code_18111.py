8a3a4390b5c7bfc5be8111305b305bcb3d7b4872
NoseSSG <dmlcks1998@naver.com>
2024-09-29 19:41:17 +0900

    백준 마인크래프트 끝

--- 18111_마인크래프트/code_18111.py
+++ 18111_마인크래프트/code_18111.py
@@ -1,2 +1,30 @@
 # https://www.acmicpc.net/problem/18111
-inp
+import sys
+# sys.stdin = open('input_18111.txt','r')
+
+N,M,B = map(int,input().split())
+mine_map = []
+for _ in range(N):
+    temp = map(int,input().split())
+    mine_map += temp
+high = max(mine_map)
+low = min(mine_map)
+timer = float('inf')
+setup_height = 0
+for height in range(low,high+1):
+    now_time = 0
+    need_block = 0
+    for now in mine_map:
+        if now < height:
+            need_block += height - now
+        else:
+            now_time += (now - height)
+    if now_time + B < need_block:
+        continue
+    now_time *= 2
+    now_time += need_block
+    if timer >= now_time:
+        timer = now_time
+        setup_height = height
+
+print(timer,setup_height)
