164bdbe5346a79c0fe0a09d2f17e8235e97b7e99
euichan <dmlcks1998@naver.com>
2024-10-15 17:42:53 +0900

    ã백준 로봇 청소기 풀이중

--- 14503_로봇 청소기/code_14503.py
+++ 14503_로봇 청소기/code_14503.py
@@ -1,1 +1,8 @@
 # https://www.acmicpc.net/problem/14503
+import sys
+sys.stdin = open('input_14503.txt','r')
+
+N,M = map(int,input().split())
+robot = list(map(int,input().split()))
+room = [list(map(int,input().split())) for _ in range(N)]
+print(room)
