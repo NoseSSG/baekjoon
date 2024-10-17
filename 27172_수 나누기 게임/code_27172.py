20c889ce5a749a2f15679296af3c7435025d014c
euichan <dmlcks1998@naver.com>
2024-10-17 17:15:59 +0900

    백준 수나누기 게임 끝

--- 27172_수 나누기 게임/code_27172.py
+++ 27172_수 나누기 게임/code_27172.py
@@ -1,5 +1,17 @@
 # https://www.acmicpc.net/problem/27172
 import sys
+
 sys.stdin = open('input_27172.txt','r')
 N = int(input())
 num = list(map(int,input().split()))
+max_num = max(num)
+cnt_num = [0] * (max_num + 1)
+S = set(num)
+for i in num:
+    if i == max_num: continue
+    for n in range(i*2,max_num+1,i):
+        if n in S:
+            cnt_num[i] += 1
+            cnt_num[n] -= 1
+for i in num:
+    print(cnt_num[i],end =' ')
