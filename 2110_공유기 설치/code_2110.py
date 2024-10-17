273e357cdeb227ddd74acdf7407df8c45318cdcd
euichan <dmlcks1998@naver.com>
2024-10-17 17:06:30 +0900

    백준 공유기 설치 끝

--- 2110_공유기 설치/code_2110.py
+++ 2110_공유기 설치/code_2110.py
@@ -1,1 +1,23 @@
 # https://www.acmicpc.net/problem/2110
+import sys
+sys.stdin = open('input_2110.txt')
+
+N,C = map(int,input().split())
+home = [int(input()) for _ in range(N)]
+home.sort()
+start = 1
+end = home[-1] - home[0]
+while start <= end:
+    mid = (start+end)//2
+    cnt = 1
+    now = home[0]
+    for i in range(1,N):
+        if home[i] >= now + mid:
+            cnt += 1
+            now = home[i]
+    if cnt >= C:
+        start = mid +1
+    else:
+        end = mid - 1
+
+print(end)
