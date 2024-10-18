e712431d36322da2b4db53428e0650dd9b7cfb6f
euichan <dmlcks1998@naver.com>
2024-10-18 13:19:32 +0900

    백준 세수의 합 끝

--- 2295_세수의 합/code_2295.py
+++ 2295_세수의 합/code_2295.py
@@ -1,1 +1,16 @@
 # https://www.acmicpc.net/problem/2295
+import sys
+sys.stdin = open('input_2295.txt','r')
+
+N = int(input())
+num = [int(input()) for _ in range(N)]
+num.sort()
+A = set()
+for a in num:
+    for b in num:
+        A.add(a+b)
+for i in range(N-1,-1,-1):
+    for j in range(i+1):
+        if num[i] - num[j] in A:
+            print(num[i])
+            exit()
