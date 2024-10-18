25460ce5d04b5644eafb84a996c896cab65b0944
euichan <dmlcks1998@naver.com>
2024-10-18 15:51:58 +0900

    백준 분산처리 끝

--- 1009_분산처리/code_1009.py
+++ 1009_분산처리/code_1009.py
@@ -1,1 +1,13 @@
 # https://www.acmicpc.net/problem/1009
+import sys
+sys.stdin = open('input_1009.txt')
+
+T = int(input())
+
+for test_case in range(T):
+    a,b = map(int,input().split())
+    ans = 1
+    for i in range(b):
+        ans = (ans*a)%10
+    if ans == 0: ans = 10
+    print(ans)
