aab215e610641658625a4ef0eacd7a8321b12d49
euichan <dmlcks1998@naver.com>
2024-10-18 16:11:58 +0900

    백준 카잉 달력 끝

--- 6064_카잉 달력/code_6064.py
+++ 6064_카잉 달력/code_6064.py
@@ -1,1 +1,18 @@
 # https://www.acmicpc.net/problem/6064
+import sys
+sys.stdin = open('input_6064.txt','r')
+
+import math
+
+T = int(input())
+for test_case in range(T):
+    N,M,x,y = map(int,input().split())
+    LCM = int((N*M)/math.gcd(N,M))
+    ans = -1
+    time = 0
+    while N*time <= LCM:
+        if ((N*time + x)-y) % M == 0:
+            ans = N*time + x
+            break
+        time += 1
+    print(ans)
