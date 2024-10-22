ba1974c483f616c679827da114a0be0b84fdd87d
euichan <dmlcks1998@naver.com>
2024-10-22 13:34:09 +0900

    백준 전자레인지 끝

--- 10162_전자레인지/code_10162.py
+++ 10162_전자레인지/code_10162.py
@@ -1,1 +1,16 @@
 # https://www.acmicpc.net/problem/10162
+import sys
+sys.stdin = open('input_10162.txt','r')
+
+N = int(input())
+if N%10 !=0 :
+    print(-1)
+else:
+    ans = []
+    ans.append(N//300)
+    N = N % 300
+    ans.append(N//60)
+    N = N % 60
+    ans.append(N//10)
+
+    print(*ans)
