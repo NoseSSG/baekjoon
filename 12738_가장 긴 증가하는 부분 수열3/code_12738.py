b0299a198b91ee10c8ab6622ef4ceae46cc2f3f3
euichan <dmlcks1998@naver.com>
2024-10-22 13:11:43 +0900

    백준 가장 긴 증가하는 부분 수열3 끝

--- 12738_가장 긴 증가하는 부분 수열3/code_12738.py
+++ 12738_가장 긴 증가하는 부분 수열3/code_12738.py
@@ -1,1 +1,16 @@
 # https://www.acmicpc.net/problem/12738
+import sys
+# sys.stdin = open('input_12738.txt','r')
+
+import bisect
+
+N = int(input())
+num = list(map(int,input().split()))
+temp = []
+for i in num:
+    idx = bisect.bisect_left(temp,i)
+    if idx == len(temp):
+        temp.append(i)
+    else:
+        temp[idx] = i
+print(len(temp))
