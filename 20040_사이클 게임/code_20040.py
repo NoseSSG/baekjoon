fa2b29bbe4e8a9cf4fecb9c1c612964aafc0bb99
euichan <dmlcks1998@naver.com>
2024-10-22 11:56:48 +0900

    백준 싸이클 게임 끝

--- 20040_사이클 게임/code_20040.py
+++ 20040_사이클 게임/code_20040.py
@@ -1,1 +1,28 @@
 # https://acmicpc.net/problem/20040
+import sys
+# sys.stdin=open('input_20040.txt','r')
+
+def find_parent(x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent[x])
+    return parent[x]
+
+
+N,M = map(int,input().split())
+ans = 0
+spot = set()
+parent = [i for i in range(N)]
+
+for i in range(1,M+1):
+
+    a,b = map(int,input().split())
+    parent_a = find_parent(a)
+    parent_b = find_parent(b)
+    if parent_a == parent_b:
+        ans = i
+        break
+    if parent_a < parent_b:
+        parent[parent_b] = parent_a
+    else:
+        parent[parent_a] = parent_b
+print(ans)
