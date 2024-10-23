8471944255f0223c8f30f8577607ce4916d07c31
euichan <dmlcks1998@naver.com>
2024-10-23 10:38:56 +0900

    백준 회전하는 큐 끝

--- 1021_회전하는 큐/code_1021.py
+++ 1021_회전하는 큐/code_1021.py
@@ -1,1 +1,23 @@
 # https://acmicpc.net/problem/1021
+import sys
+# sys.stdin = open('input_1021.txt','r')
+
+from collections import deque
+
+N,M = map(int,input().split())
+temp_list = [i for i in range(1,N+1)]
+d_que = deque(temp_list)
+numbers = list(map(int,input().split()))
+ans = 0
+for number in numbers:
+    while True:
+        if d_que[0] == number:
+            d_que.popleft()
+            break
+        if d_que.index(number) <= len(d_que)//2:
+            ans += 1
+            d_que.rotate(-1)
+        else:
+            ans += 1
+            d_que.rotate(1)
+print(ans)
