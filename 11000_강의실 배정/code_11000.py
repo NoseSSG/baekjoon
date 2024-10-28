c7b898acb7998754ea2320e88cb83922d007e8c2
euichan <dmlcks1998@naver.com>
2024-10-28 14:07:49 +0900

    백준 강의실 배정 끝

--- 11000_강의실 배정/code_11000.py
+++ 11000_강의실 배정/code_11000.py
@@ -1,1 +1,23 @@
 # https://www.acmicpc.net/problem/11000
+import sys
+sys.stdin = open('input_11000.txt','r')
+
+import heapq
+
+N = int(input())
+
+class_time = [list(map(int,input().split())) for _ in range(N)]
+class_time.sort()
+h_que = []
+heapq.heappush(h_que,class_time[0][1])
+
+ans = 1
+for i in range(1,N):
+    a,b = class_time[i]
+    if h_que[0] > a:
+        ans += 1
+        heapq.heappush(h_que,b)
+    else:
+        heapq.heappop(h_que)
+        heapq.heappush(h_que,b)
+print(ans)
