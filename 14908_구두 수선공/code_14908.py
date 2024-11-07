f10f4c31961fc9e9b21094dd02068ce7b7e1f5ed
euichan <dmlcks1998@naver.com>
2024-11-07 11:50:07 +0900

    백준 구두수선공 끝

--- 14908_구두 수선공/code_14908.py
+++ 14908_구두 수선공/code_14908.py
@@ -1,1 +1,8 @@
 # https://www.acmicpc.net/problem/14908
+import sys
+sys.stdin = open('input_14908.txt','r')
+
+N = int(input())
+shoes = [list(map(int,input().split()))+[i] for i in range(1,N+1)]
+shoes.sort(key = lambda x : (x[1]/x[0]) ,reverse=True)
+print(*list(map(lambda x :x[2],shoes)))
