3c7957b987b7a9ddbfc6365e273d89f1e613fb0f
euichan <dmlcks1998@naver.com>
2024-11-07 13:58:31 +0900

    백준 멀티탭 스케줄링 끝

--- 1700_멀티탭 스케줄링/code_1700.py
+++ 1700_멀티탭 스케줄링/code_1700.py
@@ -1,1 +1,30 @@
 # https://www.acmicpc.net/problem/1700
+import sys
+# sys.stdin = open('input_1700.txt')
+
+def find_latest(i):
+    num = 0
+    last_idx = -1
+    for plug in plugs:
+        try:
+            plug_idx = temp[i:].index(plug)
+        except:
+            return plug
+        if last_idx < plug_idx:
+            last_idx,num = plug_idx,plug
+    return num
+
+
+
+N,M = map(int,input().split())
+temp = list(map(int,input().split()))
+
+plugs = set()
+ans = 0
+for idx,value in enumerate(temp):
+    plugs.add(value)
+    if len(plugs) > N:
+        ans += 1
+        take_off = find_latest(idx)
+        plugs.discard(take_off)
+print(ans)
