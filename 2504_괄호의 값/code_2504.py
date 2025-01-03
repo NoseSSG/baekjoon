2617025d8480204d324c0d277f84ac7f57ff1fc0
euichan <dmlcks1998@naver.com>
2025-01-03 17:33:09 +0900

    백준 괄호의값 끝

--- 2504_괄호의 값/code_2504.py
+++ 2504_괄호의 값/code_2504.py
@@ -1,1 +1,42 @@
 # https://www.acmicpc.net/problem/2504
+import sys
+# sys.stdin = open('input_2504.txt')
+
+temp = list(input().rstrip())
+stack = []
+ans = 0
+now_int = 1
+
+for i in range(len(temp)):
+    if temp[i] == '(' or temp[i] == '[':
+        stack.append(temp[i])
+        if temp[i] == '(':
+            now_int *= 2
+        elif temp[i] == '[':
+            now_int *= 3
+    else:
+        if not stack:
+            ans = 0
+            break
+        last = stack.pop()
+        now = temp[i]
+        before = temp[i-1]
+        if now == ')':
+            if before == '[' or last =='[':
+                ans = 0
+                break
+            elif before == '(':
+                ans += now_int
+            now_int //= 2
+        elif now == ']':
+            if before == '(' or last == '(':
+                ans = 0
+                break
+            elif before == '[':
+                ans += now_int
+            now_int //= 3
+
+if stack:
+    print(0)
+else:
+    print(ans)
