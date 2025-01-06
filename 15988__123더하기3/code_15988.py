0310caf18139a2cdeed0676736ed26c27d8a2b27
euichan <dmlcks1998@naver.com>
2025-01-06 10:22:55 +0900

    백준 123더하기3 끝

--- 15988__123더하기3/code_15988.py
+++ 15988__123더하기3/code_15988.py
@@ -1,1 +1,17 @@
 # https://www.acmicpc.net/problem/15988
+import sys
+sys.stdin = open('input_15988.txt')
+
+N = int(input())
+
+DP = [0 for _ in range(1000001)]
+DP[1] = 1
+DP[2] = 2
+DP[3] = 4
+
+for i in range(4,1000001):
+    DP[i] = DP[i-1]%1000000009 + DP[i-2]%1000000009 + DP[i-3]%1000000009
+
+for _ in range(N):
+    N = int(input())
+    print(DP[N]%1000000009)
