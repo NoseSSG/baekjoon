858f92d9c5e1f8a50045ab5d80a88db2c3f5b56b
euichan <dmlcks1998@naver.com>
2025-01-06 16:30:43 +0900

    백준 주식 끝

--- 11501_주식/code_11501.py
+++ 11501_주식/code_11501.py
@@ -1,1 +1,17 @@
 # https://www.acmicpc.net/problem/11501
+import sys
+sys.stdin = open('input_11501.txt')
+
+test_case = int(input())
+
+for _ in range(test_case):
+    day = int(input())
+    price = list(map(int,input().split()))
+    ans = 0
+    max_price = 0
+    for n_d in range(day-1,-1,-1):
+        if price[n_d] > max_price:
+            max_price = price[n_d]
+        else:
+            ans += max_price-price[n_d]
+    print(ans)
