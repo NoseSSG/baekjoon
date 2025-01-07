4da0e64478603adcfed49ccf9ffd03b0025c4443
euichan <dmlcks1998@naver.com>
2025-01-07 09:06:05 +0900

    백준 트ㅏㅊ; 끝

--- 4378_트ㅏㅊ;/code_4378.py
+++ 4378_트ㅏㅊ;/code_4378.py
@@ -1,1 +1,17 @@
 # https://www.acmicpc.net/problem/4378
+import sys
+sys.stdin = open('input_4378.txt')
+
+keyboard = "`1234567890-=QWERTYUIOP[]\ASDFGHJKL;'ZXCVBNM,./"
+while 1:
+    try:
+        temp = input()
+        ans = ''
+        for i in temp:
+            if i == ' ' or i =='':
+                ans += i
+            else:
+                ans+=keyboard[keyboard.find(i)-1]
+        print(ans)
+    except:
+        exit()
