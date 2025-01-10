c05779d869d2ec19fc57a61ac35724fef0581459
euichan <dmlcks1998@naver.com>
2025-01-10 09:25:32 +0900

    백준 파일 합치기 끝

--- 11066_파일 합치기/code_11066.py
+++ 11066_파일 합치기/code_11066.py
@@ -1,1 +1,19 @@
 # https://www.acmicpc.net/problem/11066
+import sys
+sys.stdin = open('input_11066.txt')
+
+test_case = int(input())
+
+for _ in range(test_case):
+    M = int(input())
+    pages = [0] + list(map(int,input().split()))
+    DP = [[0]*(M+1) for _ in range(M+1)]
+    for i in range(M):
+        DP[i][i+1] = pages[i] + pages[i+1]
+    
+    for i in range(M-1,0,-1):
+        for j in range(M+1):
+            if DP[i][j]==0 and j>i:
+                DP[i][j] = min(DP[i][k] + DP[k+1][j] for k in range(i,j)) + sum(pages[i:j+1])
+
+    print(DP[1][M])
