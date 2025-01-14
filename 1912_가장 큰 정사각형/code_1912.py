083aa7edc67397e2436afa0f3265b6b20f5cd496
euichan <dmlcks1998@naver.com>
2025-01-14 15:32:03 +0900

    백준 가장 큰 정사각형 끝

--- 1912_가장 큰 정사각형/code_1912.py
+++ 1912_가장 큰 정사각형/code_1912.py
@@ -1,1 +1,24 @@
 # https://www.acmicpc.net/problem/1915
+import sys
+sys.stdin = open('input_1912.txt')
+
+N,M = map(int,input().split())
+
+temp = [list(map(int,input().rstrip())) for _ in range(N)]
+
+DP = [[0]*M for _ in range(N)]
+
+ans = 0
+
+for i in range(N):
+    for j in range(M):
+        if i==0 or j==0:
+            DP[i][j] = temp[i][j]
+        elif temp[i][j] == 0:
+            DP[i][j] = 0
+        else:
+            DP[i][j] = min(DP[i-1][j-1],DP[i][j-1],DP[i-1][j]) + 1
+
+        ans = max(ans,DP[i][j])
+print(ans**2)
+        
