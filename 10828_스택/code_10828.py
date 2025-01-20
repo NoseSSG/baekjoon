55982ce188b425c3e0dd106a845b0f0a7d9a4261
euichan <dmlcks1998@naver.com>
2025-01-20 09:40:19 +0900

    백준 스택 끝

--- 10828_스택/code_10828.py
+++ 10828_스택/code_10828.py
@@ -1,1 +1,34 @@
 # https://www.acmicpc.net/problem/10828
+import sys
+sys.stdin = open('input_10828.txt')
+
+N = int(input())
+
+stack = []
+size = 0
+
+for _ in range(N):
+    temp = list(map(str,input().split()))
+    
+    order = temp[0]
+    if order == 'push':
+        stack.append(temp[1])
+        size += 1
+    elif order == 'pop':
+        if size == 0:
+            print(-1)
+        else:
+            print(stack.pop())
+            size -= 1
+    elif order == 'size':
+        print(size)
+    elif order == 'empty':
+        if size == 0:
+            print(1)
+        else:
+            print(0)
+    elif order == 'top':
+        if size == 0:
+            print(-1)
+        else:
+            print(stack[-1])
