f42816ba12a92c214abaa30234a5d8cc0ec66cbb
euichan <dmlcks1998@naver.com>
2025-02-24 17:08:20 +0900

    백준 개미 끝;

--- 10158_개미/code_10158.py
+++ 10158_개미/code_10158.py
@@ -1,1 +1,13 @@
 # https://www.acmicpc.net/problem/10158
+import sys
+sys.stdin = open('input_10158.txt')
+
+
+w,h = map(int,input().split())
+p,q = map(int,input().split())
+t = int(input())
+
+x = w - abs(w - (p+t)%(2*w))
+y = h - abs(h - (q+t)%(2*h))
+
+print(x,y)
