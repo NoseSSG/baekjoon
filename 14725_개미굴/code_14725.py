942ec3205bad1e40e560d146337bee2fd2e081f3
euichan <dmlcks1998@naver.com>
2025-02-24 17:55:44 +0900

    백준 개미굴 끝

--- 14725_개미굴/code_14725.py
+++ 14725_개미굴/code_14725.py
@@ -1,1 +1,28 @@
 # https://www.acmicpc.net/problem/14725
+import sys
+sys.stdin = open('input_14725.txt')
+
+def add_tree(tree,small_route):
+    if len(small_route) == 0:
+        return
+    if small_route[0] not in tree:
+        tree[small_route[0]] = {}
+    add_tree(tree[small_route[0]],small_route[1:])
+    
+def print_Tree(tree,length):
+    for i in sorted(tree.keys()):
+        print('--'*length,end='')
+        print(i)
+        print_Tree(tree[i],length + 1)
+
+
+n = int(input())
+routes = [list(map(str,input().split())) for _ in range(n)]
+trees = {}
+
+
+for route in routes:
+    r = route[1:]
+    add_tree(trees,r)
+
+print_Tree(trees,0)
