a480e46d837959b9d8aa973d69d7efa687c7a1de
euichan <dmlcks1998@naver.com>
2025-02-24 16:28:37 +0900

    백준 멍멍이 쓰다듬기 끝

--- 1669_멍멍이 쓰다듬기/code_1669.py
+++ 1669_멍멍이 쓰다듬기/code_1669.py
@@ -1,1 +1,18 @@
 # https://www.acmicpc.net/problem/1669
+import sys
+sys.stdin = open('input_1669.txt')
+
+def solve(num):
+    ans = 0
+    temp = 1
+    while True:
+        for i in range(2):
+            if num <=0:
+                return ans
+            ans += 1
+            num -= temp
+        temp += 1
+
+N,M = map(int,input().split())
+diff = abs(N-M)
+print(solve(diff))
