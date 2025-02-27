6584e4069c22f80906c23b9931be8e6ebcee41de
euichan <dmlcks1998@naver.com>
2025-02-27 17:28:30 +0900

    백준 부분수열의 합 끝

--- 14225_부분수열의 합/code_14225.py
+++ 14225_부분수열의 합/code_14225.py
@@ -1,1 +1,21 @@
 # https://www.acmicpc.net/problem/14225
+import sys
+sys.stdin = open('input_14225.txt')
+
+N = int(input())
+temp = list(map(int,input().split()))
+ans = [0] * 2000000
+
+def bit_mask(num):
+    for i in range(1<<num):
+        temp_arr = []
+        for j in range(num):
+            if i & (1<<j):
+                temp_arr.append(temp[j])
+        if temp_arr:
+            ans[sum(temp_arr)] = 1
+bit_mask(N)
+for i in range(1,len(ans)+1):
+    if not ans[i]:
+        print(i)
+        break
