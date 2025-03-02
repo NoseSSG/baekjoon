b393bca910cd9c8a6c60f824b4b48afbc578afc0
euichan <dmlcks1998@naver.com>
2025-03-02 15:43:06 +0900

    백준 최솟값 끝

--- 10868_최솟값/code_10868.py
+++ 10868_최솟값/code_10868.py
@@ -7,23 +7,23 @@ def make_seg(idx,start,end):
     if start == end:
         seg_tree[idx] = num[start]
         return seg_tree[idx]
-    if start +1 == end:
-        seg_tree[idx] = min(num[start],num[end])
-        return seg_tree[idx]
+    # if start +1 == end:
+    #     seg_tree[idx] = min(num[start],num[end])
+    #     return seg_tree[idx]
     mid = (start+end)//2
     left_min = make_seg(idx*2, start, mid)
-    right_min = make_seg((idx*2)+1, mid, end)
+    right_min = make_seg((idx*2)+1, mid+1, end)
     seg_tree[idx] = min(left_min,right_min)
     return seg_tree[idx]
 
 def find_seg(idx,start,end,left,right):
-    if left >= end or right <= start:
+    if left > end or right < start:
         return float('inf')
     if left <= start and end <= right:
         return seg_tree[idx]
     mid = (start+end)//2
     left_min = find_seg(idx*2,start,mid,left,right)
-    right_min = find_seg((idx*2)+1,mid,end,left,right)
+    right_min = find_seg((idx*2)+1,mid+1,end,left,right)
     return min(left_min,right_min)
 
 
@@ -32,6 +32,6 @@ num = [int(input()) for _ in range(N)]
 range_list=[list(map(int,input().split())) for _ in range(M)]
 seg_tree =[float('inf')] * N * 4
 make_seg(1,0,N-1)
-print(seg_tree)
+# print(seg_tree)
 for a,b in range_list:
     print(find_seg(1,0,N-1,a-1,b-1))
