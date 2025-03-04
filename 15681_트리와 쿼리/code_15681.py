c72ee55bc24c206b69fa71053222a86498332ccb
euichan <dmlcks1998@naver.com>
2025-03-04 10:18:23 +0900

    백준 트리와 쿼리 끝

--- 15681_트리와 쿼리/code_15681.py
+++ 15681_트리와 쿼리/code_15681.py
@@ -1,1 +1,29 @@
 # https://www.acmicpc.net/problem/15681
+import sys
+sys.stdin = open('input_15681.txt')
+
+def make_tree(node,depth):
+    depthes[node] = depth
+    checkes[node] = True
+    count[node] = 1
+    for next_node in tree[node]:
+        if checkes[next_node]: continue
+        count[node] += make_tree(next_node,depth+1)
+    return count[node]
+
+N,R,Q = map(int,input().split())
+
+tree = [[] for _ in range(N+1)]
+depthes = [0] * (N+1)
+checkes = [False] * (N+1)
+count = [0] * (N+1)
+for _ in range(N-1):
+    U,V = map(int,input().split())
+    tree[U].append(V)
+    tree[V].append(U)
+
+make_tree(R,0)
+
+for _ in range(Q):
+    start = int(input())
+    print(count[start])
