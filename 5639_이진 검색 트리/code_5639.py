459f62ff1f37daaf2ece8c59d07d18a162574085
NoseSSG <dmlcks1998@naver.com>
2025-08-25 18:34:12 +0900

    백준 이진 검색 트리 끝

--- 5639_이진 검색 트리/code_5639.py
+++ 5639_이진 검색 트리/code_5639.py
@@ -1,1 +1,28 @@
 # https://www.acmicpc.net/problem/5639
+import sys
+sys.stdin = open('input_5639.txt','r')
+
+temp = []
+
+while(True):
+    try:
+        temp.append(int(input()))
+    except:
+        break
+
+def solution(A):
+    if len(A) == 0:
+        return
+    mid = A[0]
+    temp_left = []
+    temp_right = []
+    temp_left = A[1:]
+    for i in range(1,len(A)):
+        if A[i] > mid:
+            temp_left = A[1:i]
+            temp_right = A[i:]
+            break
+    solution(temp_left)
+    solution(temp_right)
+    print(mid)
+solution(temp)
