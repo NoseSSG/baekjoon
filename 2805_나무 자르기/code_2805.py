a3b8510eb540cb2558e1dbe3b9a581c8bb12239b
NoseSSG <dmlcks1998@naver.com>
2025-09-28 17:01:42 +0900

    백준 나무 자르기 끝

--- 2805_나무 자르기/code_2805.py
+++ 2805_나무 자르기/code_2805.py
@@ -1,1 +1,22 @@
 # https://www.acmicpc.net/problem/2805
+import sys
+sys.stdin = open('input_2805.txt')
+
+N,M = map(int,input().split())
+tree = list(map(int,input().split()))
+start,end = 1,max(tree)
+
+while start <= end:
+    mid = (start + end) // 2
+    total = 0
+
+    for i in tree:
+        if i>mid:
+            total += i-mid
+    
+    if total >= M:
+        start = mid + 1
+    else:
+        end = mid -1
+
+print(end)
