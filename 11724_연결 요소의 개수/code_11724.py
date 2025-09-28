7ea7b06676f562633e6cac345fd259bce663ab1e
NoseSSG <dmlcks1998@naver.com>
2025-09-28 17:17:02 +0900

    백준 연결 요소의 개수 끝

--- 11724_연결 요소의 개수/code_11724.py
+++ 11724_연결 요소의 개수/code_11724.py
@@ -1,1 +1,28 @@
 # https://www.acmicpc.net/problem/11724
+import sys
+sys.stdin = open('input_11724.txt')
+
+
+def DFS(node,visited):
+    visited[node] = True
+    for i in graph[node]:
+        if not visited[i]:
+            DFS(i,visited)
+
+N,M = map(int,input().split())
+
+graph = [[] for _ in range(N+1)]
+
+for _ in range(M):
+    a,b = map(int,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+count = 0
+visited = [False] * (N+1)
+for i in range(1,N+1):
+    if not visited[i]:
+        DFS(i,visited)
+        count+=1
+
+print(count)
