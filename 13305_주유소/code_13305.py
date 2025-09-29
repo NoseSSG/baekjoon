7dba46eef8c621c02b8a0a1c240a3fab220234a5
NoseSSG <dmlcks1998@naver.com>
2025-09-29 17:03:39 +0900

    백준 주유소 끝

--- 13305_주유소/code_13305.py
+++ 13305_주유소/code_13305.py
@@ -1,1 +1,15 @@
 # https://www.acmicpc.net/problem/13305
+import sys
+sys.stdin = open('input_13305.txt')
+
+N = int(input())
+meter = list(map(int,input().split()))
+price = list(map(int,input().split()))
+
+ans = 0
+min = 10**10
+for i in range(N-1):
+    if price[i] < min:
+        min = price[i]
+    ans += meter[i]*min
+print(ans)
