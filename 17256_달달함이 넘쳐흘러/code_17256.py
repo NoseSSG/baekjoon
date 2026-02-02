820b736d837ff2410d279e3cbc4499dce07556b8
NoseSSG <dmlcks1998@naver.com>
2026-02-02 16:55:06 +0900

    백준 달달함이 넘쳐흘러 끝

--- 17256_달달함이 넘쳐흘러/code_17256.py
+++ 17256_달달함이 넘쳐흘러/code_17256.py
@@ -1,1 +1,9 @@
 # https://www.acmicpc.net/problem/17256
+import sys
+sys.stdin = open('input_17256.txt')
+input = sys.stdin.readline
+
+ax,ay,az = map(int,input().split())
+cx,cy,cz = map(int,input().split())
+
+print(cx-az,cy//ay,cz-ax)
