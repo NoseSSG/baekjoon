b23564561bcc4ae6839f8e21516f6c9c4f002413
NoseSSG <dmlcks1998@naver.com>
2026-02-02 16:33:34 +0900

    백준 피보나치5 끝

--- 10870_피보나치/code_10870.py
+++ 10870_피보나치/code_10870.py
@@ -1,1 +1,14 @@
 # https://www.acmicpc.net/problem/10870
+import sys
+sys.stdin = open("input_10870.txt")
+input = sys.stdin.readline
+
+Fibo ={0:0,1:1,2:1}
+n = int(input())
+
+def find_fibo(N):
+    if N in Fibo: return Fibo[N]
+    Fibo[N] = find_fibo(N-1) + find_fibo(N-2)
+    return Fibo[N]
+
+print(find_fibo(n))
