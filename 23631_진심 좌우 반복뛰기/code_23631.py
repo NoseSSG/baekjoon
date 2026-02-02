e429ad39bed4ad5feca880f68f14a10f83bbd67b
NoseSSG <dmlcks1998@naver.com>
2026-02-02 16:21:43 +0900

    백준 진심 좌우 반복뛰기 끝

--- 23631_진심 좌우 반복뛰기/code_23631.py
+++ 23631_진심 좌우 반복뛰기/code_23631.py
@@ -1,1 +1,27 @@
 # https://www.acmicpc.net/problem/23631
+import sys
+sys.stdin = open('input_23631.txt')
+input = sys.stdin.readline
+
+T = int(input())
+for _ in range(T):
+    N,K = map(int,input().split())
+
+    distance = lambda i:(i*(i+1)*K) // 2
+    left = 0
+    right = N-1
+    while left < right:
+        mid = (left+right)//2
+        if distance(mid) > N-1 : right = mid
+        else: left = mid + 1
+    
+    cnt = left -1
+    dist = distance(cnt)
+    if cnt % 2 == 1:
+        dir = "L"
+        pos = (cnt // 2 + 1) * K -(N-1-dist)
+    else:
+        dir = "R"
+        pos = -(cnt//2) * K + (N-1-dist)
+
+    print(pos,dir)
