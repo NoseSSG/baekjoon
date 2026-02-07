50281dcda112716f5e069c8c534958f1b8086d35
NoseSSG <dmlcks1998@naver.com>
2026-02-07 15:55:53 +0900

    백준 버블 소트 끝

--- 1517_버블소트/code_1517.py
+++ 1517_버블소트/code_1517.py
@@ -1,1 +1,48 @@
 # https://www.acmicpc.net/problem/1517
+import sys
+sys.stdin = open('input_1517.txt')
+input = sys.stdin.readline
+from math import ceil,log
+
+N = int(input())
+temp = list(map(int,input().split()))
+
+sorted_temp = sorted(temp)
+
+unique = []
+prev = None
+for v in sorted_temp:
+    if prev != v:
+        unique.append(v)
+        prev = v
+
+from bisect import bisect_left
+rank = [bisect_left(unique,v) + 1 for v in temp]
+M = len(unique)
+
+seg_tree = [0]*2**(ceil(log(M,2)+1))
+
+def update(node,s,e,idx):
+    if idx < s or idx > e:
+        return
+    seg_tree[node] += 1
+    if s == e:
+        return
+    mid = (s + e ) // 2
+    update(node*2,s,mid,idx)
+    update(node*2+1,mid+1,e,idx)
+
+def query(node,s,e,l,r):
+    if r < s or e < l:
+        return 0
+    if l <= s and e <= r:
+        return seg_tree[node]
+    mid = (s + e)//2
+    return query(node*2,s,mid,l,r) + query(node*2+1,mid+1,e,l,r)
+
+ans = 0
+for x in rank:
+    if x < M:
+        ans += query(1,1,M,x+1,M)
+    update(1,1,M,x)
+print(ans)
