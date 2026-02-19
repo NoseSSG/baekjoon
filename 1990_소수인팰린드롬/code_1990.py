314528cb68e892ba330fcae0282d8881646bdaa8
NoseSSG <dmlcks1998@naver.com>
2026-02-19 19:56:05 +0900

    백준 소수인팰린드롬 끝

--- 1990_소수인팰린드롬/code_1990.py
+++ 1990_소수인팰린드롬/code_1990.py
@@ -1,1 +1,46 @@
 # https://www.acmicpc.net/problem/1990
+import sys
+sys.stdin = open('input_1990.txt')
+input = sys.stdin.readline
+import math
+
+def is_prime(x):
+    if x < 2:
+        return False
+    if x % 2 == 0:
+        return x == 2
+    r = int(math.isqrt(x))
+    d = 3
+    while d <= r:
+        if x % d == 0:
+            return False
+        d += 2
+    return True
+
+def make_pal(half):
+    s = str(half)
+    return int(s + s[-2::-1])
+
+a, b = map(int, input().split())
+if a > b:
+    a, b = b, a
+
+for i in range(max(2, a), min(10, b + 1)):
+    if is_prime(i):
+        print(i)
+
+if a <= 11 <= b:
+    print(11)
+
+for half in range(10, 100000):
+    pal = make_pal(half)
+    
+    if pal > b:
+        break
+    if pal < a:
+        continue
+    
+    if is_prime(pal):
+        print(pal)
+
+print(-1)
