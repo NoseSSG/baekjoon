d0705ff382d8f60eb3f056945c275ab3d33bf644
NoseSSG <dmlcks1998@naver.com>
2026-02-19 19:15:01 +0900

    백준 고급 여관 끝

--- 12756_고급여관/code_12756.py
+++ 12756_고급여관/code_12756.py
@@ -1,1 +1,23 @@
 # https://www.acmicpc.net/problem/12756
+import sys
+sys.stdin = open('input_12756.txt')
+input = sys.stdin.readline
+
+def use_turn(attack,health):
+    turn = health//attack
+    if health % attack:
+        turn += 1
+    return turn
+
+A = list(map(int,input().split()))
+B = list(map(int,input().split()))
+
+a_turn = use_turn(A[0],B[1])
+b_turn = use_turn(B[0],A[1])
+
+if a_turn < b_turn:
+    print("PLAYER A")
+elif a_turn > b_turn:
+    print("PLAYER B")
+else:
+    print("DRAW")
