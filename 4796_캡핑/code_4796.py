570f36f18e3d334ca2a418909b520aa9709f1c47
NoseSSG <dmlcks1998@naver.com>
2026-02-22 16:33:59 +0900

    백준 캠핑 끝

--- 4796_캡핑/code_4796.py
+++ 4796_캡핑/code_4796.py
@@ -1,1 +1,14 @@
 # https://www.acmicpc.net/problem/4796
+import sys
+sys.stdin = open('input_4796.txt')
+input = sys.stdin.readline
+case = 0
+while 1:
+    L,P,V = map(int,input().split())
+    case += 1
+    if L==P==V==0:
+        break
+    ans = 0
+    time = V // P
+    ans += time*L + min(L,V%P)
+    print(f'Case {case}: {ans}')
