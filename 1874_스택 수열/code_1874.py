71fc86b2f4d0b10333ed5eafcd9baaaacc44998f
NoseSSG <dmlcks1998@naver.com>
2026-03-27 16:30:10 +0900

    백준 스택 수열 끝

--- 1874_스택 수열/code_1874.py
+++ 1874_스택 수열/code_1874.py
@@ -1,1 +1,25 @@
 # https://www.acmicpc.net/problem/1874
+import sys
+sys.stdin = open('input_1874.txt')
+input = sys.stdin.readline
+
+N = int(input())
+temp = [int(input()) for _ in range(N)]
+ans = []
+stack = []
+now = 1
+for idx in range(N):
+    while now <= temp[idx]:
+        ans.append('+')
+        stack.append(now)
+        now += 1
+    if temp[idx] == stack[-1]:
+        ans.append('-')
+        stack.pop()
+    else:
+        ans = ['NO']
+        break
+for x in ans:
+    print(x)
+    
+    
